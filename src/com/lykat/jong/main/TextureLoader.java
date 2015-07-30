106b9b8516fa5bff032c4b875764ae5da36e8a20
spen428 <spen428@users.noreply.github.com>
2015-07-30 12:16:48 +0100

    Updated TextureLoader to use a HashMap and renamed tile textures.

--- src/com/lykat/jong/main/TextureLoader.java
+++ src/com/lykat/jong/main/TextureLoader.java
@@ -1,43 +1,67 @@
 package com.lykat.jong.main;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.lykat.jong.game.Tile;
+import com.lykat.jong.game.TileSuit;
+import com.lykat.jong.game.TileValue;
+import com.lykat.jong.game.Wall;
 
 public final class TextureLoader {
 
 	private static boolean loaded = false;
-	private static final ArrayList<Texture> textures = new ArrayList<Texture>();
+	private static final HashMap<Integer, Texture> textures = new HashMap<Integer, Texture>();
 
 	public static void load() {
-		/* Tiles */
-		ArrayList<String> paths = new ArrayList<String>();
-		for (String suit : new String[] { "s", "p", "w", "h" }) {
-			for (int value = 1; value < 10; value++) {
-				if (suit.equals("h")) {
-					if (value > 7) {
-						break;
-					}
-				} else if (value == 5) {
-					paths.add(String.format("res/tiles/%d%sd.png", value, suit));
-				}
-				paths.add(String.format("res/tiles/%d%s.png", value, suit));
-			}
+		ArrayList<Tile> tiles = new ArrayList<Tile>();
+
+		/* Red Dora Tiles */
+		tiles.add(new Tile(TileSuit.PINZU, TileValue.UU, true));
+		tiles.add(new Tile(TileSuit.WANZU, TileValue.UU, true));
+		tiles.add(new Tile(TileSuit.SOUZU, TileValue.UU, true));
+
+		/* Regular Tiles */
+		Tile[] tilez = Wall.uniqueTileSet();
+		for (Tile tile : tilez) {
+			tiles.add(tile);
 		}
-		for (String path : paths) {
+
+		for (Tile tile : tiles) {
+			String name = tile.toString();
+			String path = String.format("res/tiles/%s.png",
+					name.replace(' ', '_'));
 			Texture texture = new Texture(Gdx.files.internal(path));
 			texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-			textures.add(texture);
+			textures.put(tile.hashCode(), texture);
 		}
 	}
 
+	@Deprecated
 	public static Texture getTextureById(int id) {
 		if (!loaded) {
 			load();
 		}
-		return textures.get(id);
+
+		if (id < textures.size()) {
+			for (int key : textures.keySet()) {
+				if (id == 0) {
+					return textures.get(key);
+				}
+				id--;
+			}
+		}
+		return null;
+	}
+
+	public static Texture getTileTexture(Tile tile) {
+		if (!loaded) {
+			load();
+		}
+		return textures.get(tile.hashCode());
 	}
 
 	private TextureLoader() {
