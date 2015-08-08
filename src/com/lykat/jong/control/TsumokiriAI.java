d1b72817549515cf849266a53fd7220b40e1c2a5
spen428 <spen428@users.noreply.github.com>
2015-08-08 17:48:03 +0100

    Removed auto-generated TODOs from TsumokiriAI

--- src/com/lykat/jong/control/TsumokiriAI.java
+++ src/com/lykat/jong/control/TsumokiriAI.java
@@ -16,10 +16,6 @@ public class TsumokiriAI extends AbstractPlayerController {
 	public TsumokiriAI(String name, GameManager gameManager) {
 		super(name, gameManager);
 	}
-	
-	public void connect() {
-		super.fireEvent(GameEventType.PLAYER_CONNECT, this);
-	}
 
 	@Override
 	public void handleEvent(GameEvent event) {
@@ -37,37 +33,10 @@ public class TsumokiriAI extends AbstractPlayerController {
 				}
 			}
 		}
-		
+
 		super.tsumoKiri();
 
 		super.notifyObservers();
-	}
-
-	private void handleConfirmation(GameEvent event) {
-		GameEventType type = event.getEventType();
-		if (type == GameEventType.ABORT_4_KAN) {
-		} else if (type == GameEventType.ABORT_ALL_RIICHI) {
-		} else if (type == GameEventType.ABORT_5_KAN) {
-		} else if (type == GameEventType.ABORT_CHOMBO) {
-		} else if (type == GameEventType.ABORT_KYUUSHU_KYUUHAI) {
-		} else if (type == GameEventType.CALL_AVAILABLE) {
-		} else if (type == GameEventType.CALL_CHII) {
-		} else if (type == GameEventType.CALL_KAN) {
-		} else if (type == GameEventType.CALL_PON) {
-		} else if (type == GameEventType.CALL_RON) {
-		} else if (type == GameEventType.DECLARE_BONUS_TILE) {
-		} else if (type == GameEventType.DECLARE_KAN) {
-		} else if (type == GameEventType.DECLARE_RIICHI) {
-		} else if (type == GameEventType.DECLARE_TSUMO) {
-		} else if (type == GameEventType.DISCARD) {
-		} else if (type == GameEventType.DRAW_FROM_DEAD_WALL) {
-		} else if (type == GameEventType.DRAW_FROM_LIVE_WALL) {
-		} else if (type == GameEventType.EXHAUSTIVE_DRAW) {
-		} else if (type == GameEventType.SKIP_CALL) {
-		} else if (type == GameEventType.TURN_FINISHED) {
-		} else if (type == GameEventType.TURN_STARTED) {
-		} else {
-		}
 	}
 
 	@Override
@@ -90,43 +59,36 @@ public class TsumokiriAI extends AbstractPlayerController {
 
 	@Override
 	public boolean keyUp(int keycode) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean keyTyped(char character) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean mouseMoved(int screenX, int screenY) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean scrolled(int amount) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 }
