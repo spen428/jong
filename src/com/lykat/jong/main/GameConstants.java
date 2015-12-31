d717b58fa25e8a9c4ac2c98d077b17cfd464531f
spen428 <spen428@users.noreply.github.com>
2015-12-31 18:31:24 +0000

    Temporarily disabled "tsumohai on top" while buggy

--- src/com/lykat/jong/main/GameConstants.java
+++ src/com/lykat/jong/main/GameConstants.java
@@ -2,25 +2,25 @@ package com.lykat.jong.main;
 
 public final class GameConstants {
 
-	public static final int TURN_DELAY_MS = 150;
+    public static final int TURN_DELAY_MS = 150;
 
-	public static final int WALL_WIDTH_TILES = 17;
-	public static final int WALL_HEIGHT_TILES = 2;
+    public static final int WALL_WIDTH_TILES = 17;
+    public static final int WALL_HEIGHT_TILES = 2;
 
-	public static final int DISCARD_WIDTH_TILES = 6;
-	public static final int DISCARD_HEIGHT_TILES = 3;
+    public static final int DISCARD_WIDTH_TILES = 6;
+    public static final int DISCARD_HEIGHT_TILES = 3;
 
-	/**
-	 * The minimum number of tiles in the player's hand for the Tsumohai to be
-	 * placed on top of the hand, instead of to its side. (Should at least be
-	 * greater than 1).
-	 */
-	public static final int MIN_TILES_TSUMOHAI_ONTOP = 8;
-	public static final int MAX_DISCARDS_PER_PLAYER = 22;
-	public static final int NUM_HAND_TILES = 13;
-	public static final int MAX_OPEN_MELDS = 5;
+    /**
+     * The minimum number of tiles in the player's hand for the Tsumohai to be
+     * placed on top of the hand, instead of to its side. (Should at least be
+     * greater than 1).
+     */
+    public static final int MIN_TILES_TSUMOHAI_ONTOP = 14; // 8;
+    public static final int MAX_DISCARDS_PER_PLAYER = 22;
+    public static final int NUM_HAND_TILES = 13;
+    public static final int MAX_OPEN_MELDS = 5;
 
-	private GameConstants() {
-	}
+    private GameConstants() {
+    }
 
 }
